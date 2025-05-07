d48adce5a1d24a933283615f83f6a5a6af40a80c
trilok2703 <trilokvallamkonda@gmail.com>
2025-05-07 23:28:00 +0530

    update storybook main js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,26 @@
-
-
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions"
   ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
-  "staticDirs": [
-    "..\\public"
-  ]
+  staticDirs: [
+    "../public"
+  ],
+  webpackFinal: async (config) => {
+    config.output.publicPath = "/storybook-react-sample/";
+    return config;
+  }
 };
+
 export default config;
